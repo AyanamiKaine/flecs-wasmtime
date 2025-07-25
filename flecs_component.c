33c50b4523fd3d4cecdf16359b507df66344e726
Ayanami Kaine <patrick.grohs@hhu.de>
2025-07-25 11:22:45 +0200

    Implement Move system and enhance world creation in flecs_component.c

--- flecs_component.c
+++ flecs_component.c
@@ -9,6 +9,28 @@ One major problem we have is to correctly expose all of the flecs api to wasm in
 function we want to export. Maybe this can be generated?
 */
 
+typedef struct {
+    double x, y;
+} Position, Velocity;
+
+void Move(ecs_iter_t *it) {
+    Position *p = ecs_field(it, Position, 0);
+    const Velocity *v = ecs_field(it, Velocity, 1);
+
+    for (int i = 0; i < it->count; i ++) {
+        p[i].x += v[i].x;
+        p[i].y += v[i].y;
+        printf("%s: {%f, %f}\n", ecs_get_name(it->world, it->entities[i]), 
+            p[i].x, p[i].y);
+    }
+}
+
+/*EXAMPLE TO CHECK IF IT ACTUALLY WORKS
+
+This also give me a create idea for a test suite written in C# that uses the wasmtime to run tests for every function exported! Also to see if all examples run the flecs defines!
+We could also implement some of the flecs tests.
+*/
+
 // Implementation for: export create-world: func() -> world-handle;
 uint64_t exports_flecs_world_create_world() {
     printf("DEBUG: Creating a minimal world with ecs_mini().\n");
@@ -28,9 +50,43 @@ uint64_t exports_flecs_world_create_world() {
     ECS_IMPORT(world, FlecsSystem);
     ECS_IMPORT(world, FlecsPipeline);
     ECS_IMPORT(world, FlecsTimer);
-
+    ECS_IMPORT(world, FlecsDoc);
     printf("SUCCESS: Flecs world is fully initialized and ready!\n");
     
+
+    ECS_COMPONENT(world, Position);
+    ECS_COMPONENT(world, Velocity);
+
+    ecs_entity_t move = ecs_system(world, {
+        // Systems are entities, and by initializing the .entity field we can
+        // set some additional properties for the system like a name. While this
+        // is not mandatory, it makes a system easier to find in tools like the
+        // explorer (https://www.flecs.dev/explorer/).
+        .entity = ecs_entity(world, {
+            .name = "Move" 
+        }),
+        .query.terms = {
+            { .id = ecs_id(Position) },
+            { .id = ecs_id(Velocity), .inout = EcsIn }
+        },
+        .callback = Move
+    });
+
+    ecs_entity_t e1 = ecs_entity(world, { .name = "e1" });
+    ecs_set(world, e1, Position, {10, 20});
+    ecs_set(world, e1, Velocity, {1, 2});
+
+    ecs_entity_t e2 = ecs_entity(world, { .name = "e2" });
+    ecs_set(world, e2, Position, {10, 20});
+    ecs_set(world, e2, Velocity, {3, 4});
+
+    // This entity will not match as it does not have Position, Velocity
+    ecs_entity_t e3 = ecs_entity(world, { .name = "e3" });
+    ecs_set(world, e3, Position, {10, 20});
+
+    // Run the system
+    ecs_run(world, move, 0.0f, NULL);
+
     // Cast the pointer to a 64-bit integer to pass it across the WASM boundary.
     return (uint64_t)world;
 }
